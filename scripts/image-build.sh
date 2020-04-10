07ec0128de24bb5340ebe78c8928c315f8140a63
ssube <seansube@gmail.com>
2020-04-10 18:32:06 -0500

    feat(build): add alt tags for images

--- scripts/image-build.sh
+++ scripts/image-build.sh
@@ -4,14 +4,36 @@ set -euxo pipefail
 
 cd ${IMAGE_ROOT}
 export IMAGE_TAG="${IMAGE_NAME}:${IMAGE_VERSION}-${CI_COMMIT_REF_SLUG}"
-docker pull ${IMAGE_TAG} || true
+
+# pull the previous image from the same branch or master to leverage its layers
+docker pull ${IMAGE_TAG} || \
+  docker pull "${IMAGE_NAME}:${IMAGE_VERSION}-master" || \
+  true  # no existing image is not an error
+
+# build and test a new image
 docker build -t ${IMAGE_TAG} .
-
 docker run --rm -v $(pwd):/tests:ro --entrypoint /usr/local/bin/goss ${IMAGE_TAG} --gossfile /tests/Gossfile.yml validate
-docker push ${IMAGE_TAG}
 
-if [[ "${CI_COMMIT_REF_SLUG}" == "master" ]];
-then
-  docker tag ${IMAGE_TAG} ${IMAGE_NAME}:${IMAGE_VERSION};
-  docker push ${IMAGE_NAME}:${IMAGE_VERSION};
-fi
+# prepare to push
+function push_image() {
+  local PUSH_TAG="${1:-${IMAGE_TAG}}"
+
+  docker push ${PUSH_TAG}
+
+  if [[ "${CI_COMMIT_REF_SLUG}" == "master" ]];
+  then
+    docker tag ${PUSH_TAG} ${IMAGE_NAME}:${IMAGE_VERSION};
+    docker push ${IMAGE_NAME}:${IMAGE_VERSION};
+  fi
+}
+
+# push under the normal tag
+push_image "${IMAGE_TAG}"
+
+# push under each alt tag
+for alt_name in "${IMAGE_ALT:-}";
+do
+  ALT_TAG="${alt_name}:${IMAGE_VERSION}-${CI_COMMIT_REF_SLUG}"
+  docker tag ${IMAGE_TAG}
+  push_image "${ALT_TAG}"
+done
